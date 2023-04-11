557028f182ea501c8ae1174048f50fb6b46f7b05
utenatop <125515771+utenatop@users.noreply.github.com>
2023-04-11 12:13:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ SINGBOX_VERSION=v1.1.7
 SINGBOX_TAGS=with_wireguard,with_clash_api,with_gvisor,with_acme,with_quic
 
 # build sing-box
-git clone https://github.com/SagerNet/sing-box -b ${SINGBOX_VERSION} --depth 1
+#git clone https://github.com/SagerNet/sing-box -b ${SINGBOX_VERSION} --depth 1
+git clone https://github.com/SagerNet/sing-box
 cd sing-box
+git reset --hard 534c4928294d31e3de741c1537b09e9a5cac8775
 export CGO_ENABLED=0
 
 go build -trimpath -tags ${SINGBOX_TAGS} \
